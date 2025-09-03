7e0630704b3b2a33af25e787b6e5559cf6ee3207
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-03 22:27:22 +0530

    chat

--- backend/controllers/chatbotMessage.js
+++ backend/controllers/chatbotMessage.js
@@ -17,8 +17,6 @@ const Message = async(req, res) => {
         })
 
 
-       // Send response
-       res.status(200).json({ success: true, message: 'Message received' });
    } catch (error) {
        console.error('Error handling message:', error);
        res.status(500).json({ error: 'Internal server error' });
