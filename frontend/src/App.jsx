96b5027e2ceab4f7a820ffd5be074c1957583b12
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-04 17:19:30 +0530

    chat

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Bot from './components/Bot'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Bot />
+    </>
+  )
+}
+
+export default App
